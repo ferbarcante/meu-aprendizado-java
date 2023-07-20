0b193d9935caefea0295568be1ecf369377bd1b0
Maria Fernanda Barçante <barcantef@gmail.com>
2023-07-20 16:55:06 -0300

    date time

---
+++ exemplos/dataHora/Program.java
@@ -0,0 +1,27 @@
+package dataHora;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Program {
+    public static void main(String[] args){
+
+        LocalDate d01 = LocalDate.parse("2023-07-25");
+        LocalDateTime d02 = LocalDateTime.parse("2022-07-25T01:30:26");
+        Instant d03 = Instant.parse("2022-07-25T01:30:26Z");
+
+        LocalDate r1 = LocalDate.ofInstant(d03, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d03, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d03, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d03, ZoneId.of("Portugal"));
+
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
+
+        System.out.println("d01 dia = " + d01.getDayOfMonth());
+    }
+}
