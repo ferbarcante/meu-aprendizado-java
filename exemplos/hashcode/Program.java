84cf75d874fde8ea50c5e9beb843ab03fbe9e153
Maria Fernanda Barçante <106846835+ferbarcante@users.noreply.github.com>
2023-09-25 17:47:10 -0300

    hashcode

---
+++ exemplos/hashcode/Program.java
@@ -0,0 +1,12 @@
+package hashcode;
+
+public class Program {
+    public static void main(String args[]){
+        String a = "Maria";
+        String b = "Alex";
+        String c = "Alex";
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        System.out.println(c.hashCode());
+    }
+}
